1564df0ad92d5896383c048606f02440d91b8c78
Amith Kumar S <amith.s@in.ey.com>
2026-01-11 22:22:15 +0530

    day 6 -- mlflow implementation

--- day5/1_getting_started.py
+++ day5/1_getting_started.py
@@ -1,8 +1,13 @@
 import mlflow
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://127.0.0.1:5001")
 
 mlflow.set_experiment("Check localhost connection")
 with mlflow.start_run():
     mlflow.log_metric("test", 1)
     mlflow.log_metric("rmse", 0.29)
+
+with mlflow.start_run():
+    mlflow.log_metric("ak1", 456)
+    mlflow.log_metric("ak2", 789)
+    
